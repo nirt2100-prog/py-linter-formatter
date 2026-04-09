df6ab9daaa6ea3665dd512a221a37a6a5742f3a9
Dmytro Pustovyi <nirt2100@gmail.com>
2026-04-09 10:51:57 +0300

    fix duplication code

--- app/main.py
+++ app/main.py
@@ -10,16 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
-            for error in errors
-        ],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
@@ -28,16 +19,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 def format_linter_report(linter_report: dict) -> list:
     return [
         {
-            "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in errors_list
-            ],
+            "errors": [format_linter_error(error) for error in errors_list],
             "path": file_path,
             "status": "failed" if errors_list else "passed"
         }
